74b9824a17edba3a3b4a5449f29c4a2343611580
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2023-07-05 00:37:34 +0530

    Java program to take input (second change)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import javax.sound.sampled.EnumControl;
 import java.util.*;
 public class Main{
     public static void main(String[] args) {
+        int sum =0, a=0, b=1;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int a = input.nextInt();
-        System.out.println("your number is : " + a);
-        System.out.println("The changes has been pushed to the git repos");
-        System.out.println("Some minor changes");
-
+        System.out.println("Enter the nth term : ");
+        System.out.println(a);
+        System.out.println(b);
+        int n = input.nextInt();
+        for (int i=2; i<=n; i++){
+            a=b;
+            b=sum;
+            sum = a+b;
+            System.out.println(sum);
+        }
 
 
     }
