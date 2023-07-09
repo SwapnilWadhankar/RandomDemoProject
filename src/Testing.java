7f49e7e60b9ec58315532e4717deec6a1bf73829
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2023-07-09 22:12:18 +0530

    Tested basic inbuilt functions on Strings and Character array.

---
+++ src/Testing.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Testing {
+    public static void main(String[] args) {
+        String name = "Swapnil";
+        char[] output = name.toCharArray();
+        for(char ch : output){
+            System.out.print(ch + " ");
+        }
+
+    }
+}
