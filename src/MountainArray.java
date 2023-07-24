64141341bd3593e20d3fb9f6f2368c603a691dc7
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2023-07-24 14:40:58 +0530

    Split the array in M continuous subArray such that will get the minimum largest sum as output

--- src/MountainArray.java
+++ src/MountainArray.java
@@ -11,21 +11,14 @@ public class MountainArray {
         int end = arr.length - 1;
 
         while(start<end){
-
             int middle = start + (end - start)/2;
-
             if(arr[middle]>arr[middle+1]){
                 end = middle;
-
             }
-
             else {
                 start = middle + 1;
             }
         }
-
         return start;
-
     }
-
 }
